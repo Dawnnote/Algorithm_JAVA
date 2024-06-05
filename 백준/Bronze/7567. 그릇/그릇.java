888b0232d89fe7d2c2966402da36c846fa1d7ca6
Shoon2 <117843786+Dawnnote@users.noreply.github.com>
2024-06-05 14:56:42 +0900

    [Bronze II] Title: 그릇, Time: 124 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/7567. 그릇/그릇.java
+++ 백준/Bronze/7567. 그릇/그릇.java
@@ -10,24 +10,20 @@ public class Main {
         String str = br.readLine();
 
         Queue<String> bowl = new LinkedList<>();
-        
+
         int strRange = str.length();
         for (int i = 0; i < strRange; i++) {
             bowl.add(String.valueOf(str.charAt(i)));
         }
-
-        // 처음 그릇을 빼서 변수로 설정
-        String front = bowl.poll();
-        // 처음 그릇의 높이 10cm 초기화
-        int height = 10;
         
+        int height = 0;
         while (!bowl.isEmpty()) {
+            String front = bowl.poll();
+
             if (front.equals(bowl.peek())) {
                 height += 5;
-                front = bowl.poll();
             } else {
                 height += 10;
-                front = bowl.poll();
             }
         }
 

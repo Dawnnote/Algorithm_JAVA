ac320b9b3ffe3e10d5807a7767d55035e8e13f97
Shoon2 <117843786+Dawnnote@users.noreply.github.com>
2024-06-10 15:11:53 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 1156 ms, Memory: 114052 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static Map<Integer, ArrayList<Integer>> graph = new HashMap<>();
+    static Queue<Integer> queue = new LinkedList<>();
+    static int[] visited;
+    // 시작 정점도 순서에 포함이므로 값 1로 초기화
+    static int cnt = 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+
+        // 입력받은 n(정점)만큼 map 초기화 
+        for(int i = 1; i <= n; i++) {
+            graph.put(i, new ArrayList<Integer>());
+        }
+
+        // 방문 순서를 담아줄 리스트 초기화
+        visited = new int[n + 1];
+
+        // 입력받은 간선의 수 만큼, 간선 정보 값을 넣어줌
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int n1 = Integer.parseInt(st.nextToken());
+            int n2 = Integer.parseInt(st.nextToken());
+
+            graph.get(n1).add(n2);
+            graph.get(n2).add(n1);
+        }
+        br.close();
+
+        // value 값들을 정렬
+        for (ArrayList<Integer> value : graph.values()) {
+            Collections.sort(value);
+        }
+
+        bfs(r);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < visited.length; i++) {
+            sb.append(visited[i]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+
+    private static void bfs(int start) {
+
+        queue.add(start);
+        visited[start] = cnt++;
+
+        while(!queue.isEmpty()) {
+            int vertex = queue.poll();
+
+            for (int i = 0; i < graph.get(vertex).size(); i++) {
+                int node = graph.get(vertex).get(i);
+                if (visited[node] == 0) {
+                    queue.add(node);
+                    visited[node] = cnt++;
+                }
+            }
+        }
+    }
+}
